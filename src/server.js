1f9748d6d594bdc9642fbc4e0809fe3cc0cfb58b
DFerranti <dferrantisc@hotmail.com>
2021-06-03 23:18:30 -0400

    Confgs da Pag

--- src/server.js
+++ src/server.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const bdconex = require("./conexao");
 
+const initModels = require("./entidades/init-models");
+
 const app = express();
 
-bdconex.authenticate().then(() => console.log("Banco de Dados conectado"));
+//bdconex.authenticate().then(() => console.log("Banco de Dados conectado"));
+const entdds = initModels(bdconex);
+const user = entdds.usuariocliente.create({ nome: "alisson", cpf: "000.269.2541.01", email: "asdasd@mail.com", telefone: "14578899", datadenascimento: "10.08.1997" })
 
 app.listen(3000, () => console.log("server conectado"));
